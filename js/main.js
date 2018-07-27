787306fd2a543e9b9ddf77113c716614e0be3991
Desmond Peals <dpeals86@gmail.com>
2018-07-27 11:19:59 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//Function will reveal "Congratulations My Site Has Loaded" in console log, prompt with an alert box 'WELCOME TO DARK GOLD'//
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Congratulations My Site Has Loaded!!!');
+  alert('WELCOME TO DARK GOLD');
+})
+
